574fcbbf5937e477938d7d52525c500ac510ea06
Codingvibes8 <wecodeappz@gmail.com>
2024-12-18 00:55:06 +0000

    initial commit

---
+++ app/_constants/navItems.ts
@@ -0,0 +1,7 @@
+export const navItems = [
+  { url: "/about", title: "About" },
+  { url: "/careers", title: "Carrers" },
+  { url: "/history", title: "History" },
+  { url: "/services", title: "Services" },
+  { url: "projects", title: "Projects" },
+];
